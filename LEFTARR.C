280de5f6b6201dea5289ebbef3566f385e24efce
M-suganthi <30444732+M-suganthi@users.noreply.github.com>
2017-07-26 00:29:12 +0530

    Add files via upload

---
+++ LEFTARR.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j=0,k=0,c=0,b[100],n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(a[i]!=0)
+{
+b[j]=a[i];
+j++;
+}
+else
+k++;
+}
+for(i=0;i<j;i++)
+printf("%d",b[i]);
+for(i=j;i<n;i++)
+printf("0");
+printf("\n%d",j);
+getch();
+}
